b0fb6a90614c8bb7ed11e51239b6196695e1102a
Obed Marquez Parlapiano <obed.marquezp@gmail.com>
2023-09-09 12:16:08 +0200

    feat: (wip) get gmail data for emails

---
+++ src/server/lib/gmail.ts
@@ -0,0 +1,35 @@
+import { google } from "googleapis";
+export async function getEmails(accessToken: string, maxResults: number) {
+  const oauth2Client = new google.auth.OAuth2();
+
+  oauth2Client.setCredentials({ access_token: accessToken });
+
+  try {
+    const gmail = google.gmail({
+      version: "v1",
+      auth: oauth2Client,
+    });
+
+    let response = await gmail.users.messages.list({
+      userId: "me",
+      maxResults: maxResults,
+    });
+
+    if (response) {
+      let messageIds =
+        response.data.messages?.flatMap((msg) => (msg.id ? [msg.id] : [])) ??
+        [];
+
+      return await Promise.all(
+        messageIds.map((id) =>
+          gmail.users.messages.get({
+            userId: "me",
+            id: `${id}`,
+          }),
+        ),
+      );
+    }
+  } catch (error) {
+    return { error };
+  }
+}
