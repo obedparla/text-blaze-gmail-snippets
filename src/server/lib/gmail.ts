5d1492bfadd9b4613874730c0c5f3c7b814cf80a
Obed Marquez Parlapiano <obed.marquezp@gmail.com>
2023-09-11 02:25:29 +0200

    refactor: much better history removal with simpler code

--- src/server/lib/gmail.ts
+++ src/server/lib/gmail.ts
@@ -23,92 +23,51 @@ export async function getEmails(accessToken: string, maxResults: number) {
         response.data.messages?.flatMap((msg) => (msg.id ? [msg.id] : [])) ??
         [];
 
-      return await Promise.all(
-        messageIds.map((id) =>
-          gmail.users.messages.get({
-            userId: "me",
-            id: `${id}`,
-          }),
+      return {
+        nextPageToken: response.data.nextPageToken,
+        emails: await Promise.all(
+          messageIds.map((id) =>
+            gmail.users.messages.get({
+              userId: "me",
+              id: `${id}`,
+            }),
+          ),
         ),
-      );
+      };
     }
   } catch (error) {
     return { error };
   }
 }
 
-// From: https://stackoverflow.com/a/61552282/5993042
 export function getPlainTextEmailWithNoHistory(
   message: gmail_v1.Schema$Message,
 ) {
-  const fromEmail = getGoogleMessageEmailFromHeader("From", message);
-  const toEmail = getGoogleMessageEmailFromHeader("To", message);
-
   const parser = new ParseGmailApi();
   let plainTextEmail = parser.parseMessage(message).textPlain.trim();
 
-  // We need to remove history of email.
-  // History starts with e.g.: 'On Thu, Apr 30, 2020 at 8:29 PM John Doe <john.doe@example.com> wrote:'
-  //
-  // We also don't know who wrote the last message in history, so we use the email that
-  // we meet first: 'fromEmail' or 'toEmail'
-  const fromEmailWithArrows = `<${fromEmail}>`;
-  const toEmailWithArrows = `<${toEmail}>`;
+  // return plainTextEmail;
 
-  const isEmailWithHistory =
-    (!!fromEmail && plainTextEmail.indexOf(fromEmailWithArrows) > -1) ||
-    (!!toEmail && plainTextEmail.indexOf(toEmailWithArrows) > -1);
-
-  if (!isEmailWithHistory) {
-    return plainTextEmail;
-  }
-
-  // First history email with arrows
-  const historyEmailWithArrows = findFirstSubstring(
-    fromEmailWithArrows,
-    toEmailWithArrows,
-    plainTextEmail,
-  );
-
-  // Remove everything after `<${fromEmail}>`
-  let emailWithNoHistory = plainTextEmail.substring(
-    0,
-    plainTextEmail.indexOf(historyEmailWithArrows) +
-      historyEmailWithArrows.length,
-  );
-
-  // Remove line that contains `<${fromEmail}>`
-  const fromRegExp = new RegExp(`^.*${historyEmailWithArrows}.*$`, "mg");
-  emailWithNoHistory = emailWithNoHistory.replace(fromRegExp, "");
-
-  return emailWithNoHistory;
+  return removeHistory(plainTextEmail);
 }
 
-function getGoogleMessageEmailFromHeader(
-  headerName: string,
-  message: gmail_v1.Schema$Message,
-) {
-  const header = message.payload?.headers?.find(
-    (header) => header.name === headerName,
-  );
+function removeHistory(emailText: string) {
+  const textLines = emailText.split(/\r?\n/gm);
 
-  const headerValue = header?.value; // John Doe <john.doe@example.com>
+  let textLinesWithNoQuotes = [];
 
-  if (!header || !headerValue) {
-    return null;
+  for (let line of textLines) {
+    if (!line.startsWith(">") && line) {
+      textLinesWithNoQuotes.push(line);
+    }
   }
 
-  const email = headerValue.substring(
-    headerValue.lastIndexOf("<") + 1,
-    headerValue.lastIndexOf(">"),
-  );
+  if (
+    textLinesWithNoQuotes.length > 0 &&
+    textLinesWithNoQuotes[textLinesWithNoQuotes.length - 1].endsWith("> wrote:")
+  ) {
+    textLinesWithNoQuotes.pop();
+  }
 
-  return email;
-}
-
-function findFirstSubstring(strA: string, strB: string, str: string) {
-  if (str.indexOf(strA) === -1) return strB;
-  if (str.indexOf(strB) === -1) return strA;
-
-  return str.indexOf(strA) < str.indexOf(strB) ? strA : strB;
+  return textLinesWithNoQuotes.join("\r\n");
 }
