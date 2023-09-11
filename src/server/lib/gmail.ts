c6a07ab6ae3b72755839d2cb5ec60f93142707e6
Obed Marquez Parlapiano <obed.marquezp@gmail.com>
2023-09-11 17:37:22 +0200

    feat (wip): refresh login token

--- src/server/lib/gmail.ts
+++ src/server/lib/gmail.ts
@@ -1,10 +1,51 @@
 import { gmail_v1, google } from "googleapis";
 import { ParseGmailApi } from "gmail-api-parse-message-ts";
+import { OAuth2Client } from "google-auth-library";
 
-export async function getEmails(accessToken: string, maxResults: number) {
+async function isTokenValid(oauth2Client: OAuth2Client, accessToken: string) {
+  try {
+    const tokenInfo = await oauth2Client.getTokenInfo(accessToken);
+    return (
+      tokenInfo && tokenInfo.expiry_date && tokenInfo.expiry_date > Date.now()
+    );
+  } catch (error) {
+    return false;
+  }
+}
+
+async function refreshAccessTokenIfNeeded(
+  oauth2Client: OAuth2Client,
+  accessToken: string,
+) {
+  if (!(await isTokenValid(oauth2Client, accessToken))) {
+    const newTokens = await oauth2Client.refreshAccessToken();
+
+    oauth2Client.setCredentials(newTokens.credentials);
+    return newTokens.credentials.access_token;
+  } else {
+    return accessToken;
+  }
+}
+
+export async function getEmails(req: Express.Request, maxResults: number) {
   const oauth2Client = new google.auth.OAuth2();
 
+  const accessToken = req.user.accessToken;
+
+  if (!accessToken) return null;
+
   oauth2Client.setCredentials({ access_token: accessToken });
+
+  const maybeNewAccessToken = await refreshAccessTokenIfNeeded(
+    oauth2Client,
+    accessToken,
+  );
+
+  if (!maybeNewAccessToken) {
+    return null;
+  }
+
+  req.user.accessToken = maybeNewAccessToken;
 
   try {
     const gmail = google.gmail({
